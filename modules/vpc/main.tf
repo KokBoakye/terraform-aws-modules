25277585ed8458cb774250aba4a123fd6b7dcf2e
KokBoakye <133584570+KokBoakye@users.noreply.github.com>
2025-08-11 15:13:06 +0100

    updated main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -5,26 +5,31 @@ resource "aws_vpc" "master_vpc" {
     }
 }
 
+# One public subnet per environment
 resource "aws_subnet" "public_subnet" {
-    vpc_id = aws_vpc.master_vpc.id
-    cidr_block = var.public_subnet_cidr_block[count.index]
-    map_public_ip_on_launch = true
-    availability_zone = var.availability_zones[count.index]
-    count = length(var.environment)
-    tags = {
-        name = "${var.environment[count.index]}_${var.user}_public_subnet"
-    }
+  count = length(var.environment)
 
-}
-
-resource "aws_subnet" "private_subnet" {
-  count             = length(var.environment) * length(var.availability_zones)  # 2 subnets per environment
   vpc_id            = aws_vpc.master_vpc.id
-  cidr_block        = var.private_subnet_cidr_block[count.index]
-  availability_zone = var.availability_zones[count.index % length(var.availability_zones)]
+  cidr_block        = var.public_subnet_cidr_block[count.index]
+  availability_zone = var.availability_zones[count.index]
 
   tags = {
-    name = "${var.environment[count.index % length(var.environment)]}_${var.user}_private_subnet_${count.index}"
+    Name = "${var.environment[count.index]}_${var.user}_public_subnet"
+  }
+}
+
+# Two private subnets per environment
+resource "aws_subnet" "private_subnet" {
+  count = length(var.environment) * 2  # 2 private subnets per env
+
+  vpc_id     = aws_vpc.master_vpc.id
+  cidr_block = var.private_subnet_cidr_block[count.index]
+
+  # Assign AZ based on environment index (integer division)
+  availability_zone = var.availability_zones[count.index / 2]
+
+  tags = {
+    Name = "${var.environment[count.index / 2]}_${var.user}_private_subnet_${count.index % 2 + 1}"
   }
 }
 

f586c36ff7ade1337a0f9b73a69af7bfce8c2892
KokBoakye <133584570+KokBoakye@users.noreply.github.com>
2025-08-13 17:53:39 +0100

    maintg

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -111,16 +111,16 @@ resource "aws_lb" "project_x_lb" {
 #     }
 # }
 
-# resource "aws_lb_listener" "project_x_listener" {
-#     load_balancer_arn = aws_lb.project_x_lb.arn
-#     port = 80
-#     protocol = "HTTP"
+resource "aws_lb_listener" "project_x_listener" {
+    load_balancer_arn = aws_lb.project_x_lb.arn
+    port = 80
+    protocol = "HTTP"
 
-#     default_action {
-#         type = "forward"
-#         target_group_arn = aws_lb_target_group.new_x_target_group.arn
-#     }
-# }
+    default_action {
+        type = "forward"
+        target_group_arn = aws_lb_target_group.main_x_target_group.arn
+    }
+}
 
 resource "aws_lb_target_group_attachment" "project_x_target_attachment" {
     count = length(var.environment)
@@ -147,23 +147,23 @@ resource "aws_lb_target_group" "main_x_target_group" {
     }
 }
 
-# # resource "aws_lb_target_group" "project_x_target_group" {
-# #     port     = 80
-# #     protocol = "HTTP"
-# #     vpc_id   = aws_vpc.master_vpc.id
+# resource "aws_lb_target_group" "project_x_target_group" {
+#     port     = 80
+#     protocol = "HTTP"
+#     vpc_id   = aws_vpc.master_vpc.id
 
-# #     health_check {
-# #         path                = "/"
-# #         interval            = 30
-# #         timeout             = 5
-# #         healthy_threshold  = 2
-# #         unhealthy_threshold = 2
-# #     }
+#     health_check {
+#         path                = "/"
+#         interval            = 30
+#         timeout             = 5
+#         healthy_threshold  = 2
+#         unhealthy_threshold = 2
+#     }
 
-# #     tags = {
-# #         Name = "project_x_target_group"
-# #     }
-# # }
+#     tags = {
+#         Name = "project_x_target_group"
+#     }
+# }
 
 # resource "aws_lb_target_group" "new_x_target_group" {
 #     port     = 2000

f4ed8fa95fdcba556825291c97185286ad618e2c
KokBoakye <133584570+KokBoakye@users.noreply.github.com>
2025-08-13 17:13:18 +0100

    targe group port change

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -100,20 +100,9 @@ resource "aws_lb" "project_x_lb" {
     name = "project-x-lb"
 }
 
-resource "aws_lb_listener" "project_x_listener" {
-    load_balancer_arn = aws_lb.project_x_lb.arn
-    port = 8080
-    protocol = "HTTP"
-
-    default_action {
-        type = "forward"
-        target_group_arn = aws_lb_target_group.project_x_target_group.arn
-    }
-}
-
 # resource "aws_lb_listener" "project_x_listener" {
 #     load_balancer_arn = aws_lb.project_x_lb.arn
-#     port = 80
+#     port = 8080
 #     protocol = "HTTP"
 
 #     default_action {
@@ -121,6 +110,17 @@ resource "aws_lb_listener" "project_x_listener" {
 #         target_group_arn = aws_lb_target_group.project_x_target_group.arn
 #     }
 # }
+
+resource "aws_lb_listener" "project_x_listener" {
+    load_balancer_arn = aws_lb.project_x_lb.arn
+    port = 80
+    protocol = "HTTP"
+
+    default_action {
+        type = "forward"
+        target_group_arn = aws_lb_target_group.new_x_target_group.arn
+    }
+}
 
 resource "aws_lb_target_group_attachment" "project_x_target_attachment" {
     count = length(var.environment)
@@ -131,6 +131,24 @@ resource "aws_lb_target_group_attachment" "project_x_target_attachment" {
 
 resource "aws_lb_target_group" "project_x_target_group" {
     port     = 5000
+    protocol = "HTTP"
+    vpc_id   = aws_vpc.master_vpc.id
+
+    health_check {
+        path                = "/"
+        interval            = 30
+        timeout             = 5
+        healthy_threshold  = 2
+        unhealthy_threshold = 2
+    }
+
+    tags = {
+        Name = "project_x_target_group"
+    }
+}
+
+resource "aws_lb_target_group" "new_x_target_group" {
+    port     = 2000
     protocol = "HTTP"
     vpc_id   = aws_vpc.master_vpc.id
 

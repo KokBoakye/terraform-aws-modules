c998b837216f68f7416c05c8660a7ad8d2b43329
KokBoakye <133584570+KokBoakye@users.noreply.github.com>
2025-08-13 14:52:14 +0100

    updated flask app port

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -8,15 +8,15 @@ output "web_server_instance_ids" {
   value       = aws_instance.web_server[*].id
 }
 
-output "bastion_host_instance_id" {
-  description = "ID of the Bastion Host EC2 instance"
-  value       = aws_instance.bastion_host[*].id
-}
+# output "bastion_host_instance_id" {
+#   description = "ID of the Bastion Host EC2 instance"
+#   value       = aws_instance.bastion_host[*].id
+# }
 
-output "bastion_host_public_ip" {
-  description = "Public IP of the Bastion Host EC2 instance"
-  value       = aws_instance.bastion_host[*].public_ip
-}
+# output "bastion_host_public_ip" {
+#   description = "Public IP of the Bastion Host EC2 instance"
+#   value       = aws_instance.bastion_host[*].public_ip
+# }
 
 output "db_endpoint" {
   value       = aws_db_instance.db_instance[*].address

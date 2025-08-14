facf2513a293bd1ee366ebe0a1a6e84c0bd85334
KokBoakye <133584570+KokBoakye@users.noreply.github.com>
2025-08-14 16:45:49 +0100

    added workflow dispatch

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -102,41 +102,41 @@ resource "aws_lb" "project_x_lb" {
 
 
 
-resource "aws_lb_listener" "project_x_listener" {
-    load_balancer_arn = aws_lb.project_x_lb.arn
-    port = 80
-    protocol = "HTTP"
+# resource "aws_lb_listener" "project_x_listener" {
+#     load_balancer_arn = aws_lb.project_x_lb.arn
+#     port = 80
+#     protocol = "HTTP"
 
-    default_action {
-        type = "forward"
-        target_group_arn = aws_lb_target_group.main_x_target_group.arn
-    }
+#     default_action {
+#         type = "forward"
+#         target_group_arn = aws_lb_target_group.main_x_target_group.arn
+#     }
+# }
+
+# resource "aws_lb_target_group_attachment" "project_x_target_attachment" {
+#     count = length(var.environment)
+#     target_group_arn = aws_lb_target_group.main_x_target_group.arn
+#     target_id = var.web_server_instance_ids[count.index]
+#     port = 8000
 }
 
-resource "aws_lb_target_group_attachment" "project_x_target_attachment" {
-    count = length(var.environment)
-    target_group_arn = aws_lb_target_group.main_x_target_group.arn
-    target_id = var.web_server_instance_ids[count.index]
-    port = 8000
-}
+# resource "aws_lb_target_group" "main_x_target_group" {
+#     port     = 8000
+#     protocol = "HTTP"
+#     vpc_id   = aws_vpc.master_vpc.id
 
-resource "aws_lb_target_group" "main_x_target_group" {
-    port     = 8000
-    protocol = "HTTP"
-    vpc_id   = aws_vpc.master_vpc.id
+#     health_check {
+#         path                = "/"
+#         interval            = 30
+#         timeout             = 5
+#         healthy_threshold  = 2
+#         unhealthy_threshold = 2
+#     }
 
-    health_check {
-        path                = "/"
-        interval            = 30
-        timeout             = 5
-        healthy_threshold  = 2
-        unhealthy_threshold = 2
-    }
-
-    tags = {
-        Name = "project_x_target_group"
-    }
-}
+#     tags = {
+#         Name = "project_x_target_group"
+#     }
+# }
 
 
 

22cdc6f798464efa314f4e96b355d7e35ff8d3ed
KokBoakye <133584570+KokBoakye@users.noreply.github.com>
2025-08-21 12:29:57 +0100

    using ami data sources

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -29,7 +29,31 @@ resource "aws_instance" "web_server" {
         sudo apt-get install -y nginx
         sudo systemctl start nginx
         sudo systemctl enable nginx
-        sudo echo "<h1>Hello from $(var.environment[count.index]) Web Server</h1>" > /var/www/html/index.html
+
+  
+        # Install prerequisite packages
+        sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+
+        # Add Docker’s official GPG key
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+        # Add Docker repository
+        echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+        $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+        # Update package info again
+        sudo apt-get update
+
+        # Install Docker
+        sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+        # Start Docker and enable it on boot
+        sudo systemctl start docker
+        sudo systemctl enable docker
+
+       
+        
     EOF
       
 }

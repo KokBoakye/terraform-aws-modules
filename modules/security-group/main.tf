9dfd506e7e570ddafa1cdde49f6ac60ac83e616a
KokBoakye <133584570+KokBoakye@users.noreply.github.com>
2025-08-22 12:22:13 +0100

    workflow dispatch

--- modules/security-group/main.tf
+++ modules/security-group/main.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "app_sg" {
         protocol    = "tcp"
         from_port   = 22
         to_port     = 22
-        security_groups = [aws_security_group.bastion.id]
+        security_groups = [aws_security_group.bastion_sg.id]
     }
 
     ingress {
